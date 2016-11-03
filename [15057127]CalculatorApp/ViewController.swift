e10520a85a90205bbbbf8c2c7651328b8142cb43
Etienne_Stev <macbookpro@macbook-pro.local>
2016-11-03 16:20:33 +0200

    Initial Commit

---
+++ [15057127]CalculatorApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  [15057127]CalculatorApp
+//
+//  Created by Etienne_Stev on 03/11/16.
+//  Copyright © 2016 Etienne_Stev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
